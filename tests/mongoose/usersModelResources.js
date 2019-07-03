ffefeb396ffd14d817c73671e17d73de5230515a
mark-marshall <mark.marshallgp@gmail.com>
2019-07-03 08:55:01 +0100

    Added tests for contacts schema

--- tests/mongoose/usersModelResources.js
+++ tests/mongoose/usersModelResources.js
@@ -36,6 +36,73 @@ const usersModelResources = {
     phone: '+447557689300',
     password: 'password',
   },
+  missing_first_name_contacts: {
+    first_name: 'Jason',
+    last_name: 'Jones',
+    email: 'jason@gmail.com',
+    phone: '+447557689301',
+    password: 'password',
+    contacts: [
+      {
+        last_name: 'Pollard',
+        level: 'Friend',
+      },
+    ],
+  },
+  missing_last_name_contacts: {
+    first_name: 'Jason',
+    last_name: 'Jones',
+    email: 'jasonj@gmail.com',
+    phone: '+447557689301',
+    password: 'password',
+    contacts: [
+      {
+        first_name: 'Sue',
+        level: 'Friend',
+      },
+    ],
+  },
+  missing_level_contacts: {
+    first_name: 'Jason',
+    last_name: 'Jones',
+    email: 'jasonj@gmail.com',
+    phone: '+447557689301',
+    password: 'password',
+    contacts: [
+      {
+        first_name: 'Sue',
+        last_name: 'Pollard',
+      },
+    ],
+  },
+  invalid_level_contacts: {
+    first_name: 'Jason',
+    last_name: 'Jones',
+    email: 'jasonj@gmail.com',
+    phone: '+447557689301',
+    password: 'password',
+    contacts: [
+      {
+        first_name: 'Sue',
+        last_name: 'Pollard',
+        level: 'Beep',
+      },
+    ],
+  },
+  valid_contacts: {
+    first_name: 'Jason',
+    last_name: 'Jones',
+    email: 'jasonj@gmail.com',
+    phone: '+447557689301',
+    password: 'password',
+    contacts: [
+      {
+        first_name: 'Sue',
+        last_name: 'Pollard',
+        level: 'Friend',
+      },
+    ],
+  },
 };
 
 module.exports = usersModelResources;
